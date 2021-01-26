42abd22aa19f0262f1d2a7f635d2b8fd2793cc6f
Maciej Lagowski <m_lagowski@o2.pl>
2021-01-26 12:09:36 +0100

    test name change

--- src/test/java/io/github/maciejlagowski/goldbank/model/service/RiskServiceTest.java
+++ src/test/java/io/github/maciejlagowski/goldbank/model/service/RiskServiceTest.java
@@ -1,16 +1,17 @@
-package io.github.maciejlagowski.goldbank.test.unit.credit.risk;
+package io.github.maciejlagowski.goldbank.model.service;
 
 import io.github.maciejlagowski.goldbank.model.database.entity.Income;
 import io.github.maciejlagowski.goldbank.model.enumeration.Industry;
 import io.github.maciejlagowski.goldbank.model.enumeration.Risk;
-import io.github.maciejlagowski.goldbank.model.service.RiskService;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class RiskCalculationTest {
+public class RiskServiceTest {
+
+    private final RiskService riskService = new RiskService();
 
     @Test
     public void incomesTest() {
@@ -19,7 +20,7 @@ public class RiskCalculationTest {
         //avg(2000 - medium; COMPUTER - low) => medium
         incomes.add(new Income(1L, 1000.0, "TestCompany2", Industry.FOOD));
         //avg(1000 - high; FOOD - high) => high
-        Risk risk = new RiskService().calculateIncomesRisk(incomes);
+        Risk risk = riskService.calculateIncomesRisk(incomes);
         //best of(medium; high) => medium
         Assert.assertEquals(Risk.MEDIUM, risk);
     }
